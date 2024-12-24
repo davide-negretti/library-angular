32b598eda301fe13f4842d5ba4f455d764558dc6
Davide Negretti <davide.negretti@gmail.com>
2024-12-24 13:17:55 +0100

    ESLint: use strict mode

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,8 @@ module.exports = tseslint.config(
     files: ["**/*.ts"],
     extends: [
       eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
       ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
