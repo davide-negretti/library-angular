cae21ec3e2e5a03b2191c2066f983672c438f082
Davide Negretti <davide.negretti@gmail.com>
2024-12-24 13:41:08 +0100

    Author service: add getById() method

--- src/app/services/rest/author.service.ts
+++ src/app/services/rest/author.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Author } from '../../models/author.model';
 import { PaginatedResponse } from '../../models/paginated-response.model';
 
@@ -7,8 +8,11 @@ import { PaginatedResponse } from '../../models/paginated-response.model';
   providedIn: 'root',
 })
 export class AuthorService {
-
   private http = inject(HttpClient);
+
+  getById(id: string): Observable<Author> {
+    return this.http.get<Author>('http://localhost:3000/authors/' + id);
+  }
 
   find(query: string, startFrom = 0, pageSize = 20) {
     let params: HttpParams = new HttpParams().set('q', query);
@@ -20,5 +24,4 @@ export class AuthorService {
     }
     return this.http.get<PaginatedResponse<Author>>('http://localhost:3000/authors/search', { params });
   }
-
 }
