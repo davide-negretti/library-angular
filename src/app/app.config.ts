a9c28dee596fa46d2d5755db2ccff6cd9e6da295
Davide Negretti <davide.negretti@gmail.com>
2024-12-24 13:25:59 +0100

    PrimeNG: base configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
+  ],
 };
